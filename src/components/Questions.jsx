5c13cd9917e5c0841440977f12523bc8435d611b
Felipe <prof.luifel@gmail.com>
2023-03-01 09:34:21 -0300

    Criando componentes de estilo

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,28 +1,64 @@
-import '../css/Questions.css'
 import play from '../assets/seta_play.png'
+import styled from 'styled-components'
 
 export default function Questions() {
   return (
     <main>
-      <div className="questions">
-        <div className="question">
+      <ContainerQuestions >
+        <Question>
           <p>Pergunta 1</p>
           <img src={play} alt="play" />
-        </div>
-        <div className="question">
+        </Question>
+        <Question>
           <p>Pergunta 1</p>
           <img src={play} alt="play" />
-        </div>
-        <div className="question">
+        </Question>
+        <Question>
           <p>Pergunta 1</p>
           <img src={play} alt="play" />
-        </div>
-        <div className="question">
+        </Question>
+        <Question>
           <p>Pergunta 1</p>
           <img src={play} alt="play" />
-        </div>
-      </div>
+        </Question>
+        <Question>
+          <p>Pergunta 1</p>
+          <img src={play} alt="play" />
+        </Question>
+      
+      </ContainerQuestions>
     </main>
     
   )
 }
+
+const ContainerQuestions = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-between; */
+  align-items: center;
+  width: 380px;
+  padding: 40px 0 60px 0;
+  background: #FB6B6B;
+`
+const Question = styled.div `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+  height: 65px;
+  margin: 13px 0;
+  padding: 0 15px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  background: white;
+  img {
+    width: 20px;
+    height: 20px;
+  }
+  p {
+    font-size: 16px;
+    font-weight: 700;
+    font-family: 'Recursive';
+  }
+`
